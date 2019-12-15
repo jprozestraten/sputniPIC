11bc2ce3da456ab21a7c25f0de95d5bbd4791c39
jean_paul_ <jean_paul_@users.noreply.github.com>
2019-12-15 23:53:41 +0100

    Debug verification

--- include/Verification.h
+++ include/Verification.h
@@ -1,6 +1,8 @@
 #ifndef VERIFICATION_H
 #define VERIFICATION_H
 
+#include "PrecisionTypes.h"
+
 /** verify results by checking idn->rhon against /testdata/rho_net */
 bool verifyRhonet(struct grid*, struct interpDensNet*, FPinterp tol = 1e-6);
 
